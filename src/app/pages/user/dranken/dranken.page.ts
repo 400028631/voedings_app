f0bfe4ea72082ccd7c0e91e5439ac28b599dcddc
freekieh <freekiehsoes@gmail.com>
2019-06-21 14:19:33 +0200

    format code

--- src/app/pages/user/dranken/dranken.page.ts
+++ src/app/pages/user/dranken/dranken.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dranken.page.scss'],
 })
 export class DrankenPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
