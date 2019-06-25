5ad337b4bde8f228bfc65a40a6581d9b803f3f59
Laurens <laurens.vg@live.nl>
2019-06-25 12:58:02 +0200

    Item pagina wordt ingevuld vanuit test data en ngfor gefixt

--- src/app/modules/components/components.module.ts
+++ src/app/modules/components/components.module.ts
@@ -6,9 +6,9 @@ import { IonicModule } from '@ionic/angular';
 import { MenuPage } from 'src/app/pages/user/menu/menu.page';
 import { DrankenPage } from 'src/app/pages/user/dranken/dranken.page';
 import { GeschiedenisPage } from 'src/app/pages/user/geschiedenis/geschiedenis.page';
-
+import { CommonModule} from '@angular/common';
 @NgModule({
-  imports: [IonicModule.forRoot()],
+  imports: [IonicModule.forRoot(), CommonModule],
   declarations: [
     UserHeaderComponent,
     MenuPage,
--- src/app/pages/user/menu/item/item.page.ts
+++ src/app/pages/user/menu/item/item.page.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemPage implements OnInit {
 
+  testData = {
+    naam: 'Fruitsalade',
+    beschrijving: 'Een heerlijke fruitsalade van het ziekenhuis',
+    maaltijd: 'lunch',
+    fotoPath: null,
+    voedingswaarden: [
+        {
+          naam: 'Vet',
+          regels: [
+            'Vet totaal 0,9 g',
+          ]
+        },
+        {
+          naam: 'Eiwit',
+          regels: [
+            'Eiwit plantaardig 6 g',
+            'Eiwit totaal 7g'
+          ]
+        },
+        {
+          naam: 'Koolhydraten',
+          regels: [
+            'koolhydraten 23,1 g'
+          ]
+        }
+    ],
+    ingredienten: [
+      'Ananas',
+      'Druiven',
+      'Kiwi\'s',
+      'Aardbeien'
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
+    // this.testData.ingredienten.forEach(ingredient => {
+    //   this.ingredient.push(ingredient);
+    // });
   }
 
 }
