5bf20626757e724712ef674fbb72d3c9beb45976
Laurens <laurens.vg@live.nl>
2019-06-25 13:53:21 +0200

    Meerdere gerechten toegevoegd

--- src/app/pages/user/menu/item/item.page.ts
+++ src/app/pages/user/menu/item/item.page.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemPage implements OnInit {
 
-  testData = {
+  testData = [{
     naam: 'Fruitsalade',
     beschrijving: 'Een heerlijke fruitsalade van het ziekenhuis',
     maaltijd: 'lunch',
-    fotoPath: null,
+    fotoPath: 'https://www.francescakookt.nl/wp-content/uploads/2017/08/zomerse_fruitsalade_uitgelicht_1.jpg',
     voedingswaarden: [
         {
           naam: 'Vet',
@@ -39,7 +39,39 @@ export class ItemPage implements OnInit {
       'Kiwi\'s',
       'Aardbeien'
     ]
-  };
+  },
+  {
+    naam: 'Yoghurt van jersey koeien',
+    beschrijving: 'Romige yoghurt van jersey koeien gereseveerd met muesli of granola en honing',
+    maaltijd: 'lunch',
+    fotoPath: 'https://hollandjersey.com/wp-content/uploads/2017/01/product-jersey-yoghurt-1.png',
+    voedingswaarden: [
+        {
+          naam: 'Vet',
+          regels: [
+            'Vet totaal 0,9 g',
+          ]
+        },
+        {
+          naam: 'Eiwit',
+          regels: [
+            'Eiwit plantaardig 6 g',
+            'Eiwit totaal 7g'
+          ]
+        },
+        {
+          naam: 'Koolhydraten',
+          regels: [
+            'koolhydraten 23,1 g'
+          ]
+        }
+    ],
+    ingredienten: [
+      'muesli',
+      'granola',
+      'honing'
+    ]
+  }];
 
   constructor() { }
 
