deffcc808c219b423e5d0b259367ebc8f72a68b0
Laurens <laurens.vg@live.nl>
2019-06-28 09:52:32 +0200

    begin aan het ingredienten pagina

--- src/app/pages/user/menu/item/ingredienten/ingredienten.page.ts
+++ src/app/pages/user/menu/item/ingredienten/ingredienten.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ingredienten',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredienten.page.scss'],
 })
 export class IngredientenPage implements OnInit {
+  @Input() ingredienten: any;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.ingredienten);
   }
 
 }
