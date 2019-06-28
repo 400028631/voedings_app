ed44f82b814619417e96d1d3c4eb1e3771d81300
freekieh <freekiehsoes@gmail.com>
2019-06-28 09:57:49 +0200

    fixed merge conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,5 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  //No route redirects to login
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  //login page route
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-
-  //user page route
-  { path: 'user', loadChildren: './pages/user/user.module#UserPageModule' },
-
-  {
-    path: 'menu-item',
-    loadChildren:
-      './pages/import/menu-item/menu-item.module#MenuItemPageModule',
-  },
-
-  //route under here should be 404
-  { path: '**', redirectTo: 'login', pathMatch: 'full' },  { path: 'menu-view', loadChildren: './pages/user/menu/menu-view/menu-view.module#MenuViewPageModule' },
-  { path: 'logpagina', loadChildren: './logpagina/logpagina.module#LogpaginaPageModule' },
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
->>>>>>> e92364ce02b1a58f23fadd64391bff648da9bc5a
 
 const routes: Routes = [
   //No route redirects to login
