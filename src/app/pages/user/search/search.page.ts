70883d2f46cfcce78b1c86a58ab19fad66f8ad59
freekieh <freekiehsoes@gmail.com>
2019-07-01 09:28:56 +0200

    added basic search page

--- src/app/pages/user/search/search.page.ts
+++ src/app/pages/user/search/search.page.ts
@@ -1,3 +1,5 @@
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+  searchTerm: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((output) => {
+      var searchterm = output.get('term');
+      this.searchTerm = searchterm;
+      this.loadItems();
+    });
   }
 
+  loadItems() {}
+
+  back() {
+    this.location.back();
+  }
 }
